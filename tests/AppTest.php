f6cc97fb543b93832716253d63aefd1edc94b2da
Nikita Arkhipov <niktyper13@gmail.com>
2019-02-08 16:13:22 +0000

    init app

---
+++ tests/AppTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+    public function testSome()
+    {
+        $this->assertEquals(true, true);
+    }
+}
