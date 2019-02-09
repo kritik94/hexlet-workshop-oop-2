977368402d8fff2a565864bca2c207068a09d921
Nikita Arkhipov <niktyper13@gmail.com>
2019-02-09 13:36:24 +0000

    add pipe example

--- src/Cli.php
+++ src/Cli.php
@@ -7,6 +7,7 @@ use \GuzzleHttp\ClientInterface;
 use \GuzzleHttp\Client;
 use \App\GeoIpApi\ApiInterface;
 use \App\GeoIpApi\IpApiService;
+use Stringy\Stringy;
 
 class Cli
 {
@@ -25,6 +26,30 @@ class Cli
             echo $getGeo->getInfoByIp($ip, [
                 'format' => $toJson ? 'json' : 'string'
             ]) . PHP_EOL;
+        };
+    }
+
+    public static function createPipe()
+    {
+        return function ($argv) {
+            $optind = null;
+            $opts = collect(getopt('', [], $optind));
+            $posArgs = array_slice($argv, $optind);
+
+            $directoryPath = $posArgs[0] ?? '.';
+
+            $sortedFiles = collect(scandir($directoryPath))
+                ->filter(function (string $name) {
+                    return true;
+                })
+                ->sort();
+
+            $medianIndex = round($sortedFiles->count() / 2);
+            $medianFilename = Stringy::create($sortedFiles[$medianIndex])
+                ->ensureRight('s')
+                ->toUpperCase();
+
+            echo $medianFilename . PHP_EOL;
         };
     }
 }
